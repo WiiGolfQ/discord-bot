06d3da64fe9a5d177bd6f1d2bc3b1f19791598f4
vncz14 <57498723+vncz14@users.noreply.github.com>
2024-03-08 04:34:20 -0500

    change default api url

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ def get_secret(key, default):
     return value
 
 DISCORD_BOT_TOKEN = get_secret("DISCORD_BOT_TOKEN", None)
-API_URL = get_secret("API_URL", "http://backend:8000/api")
+API_URL = get_secret("API_URL", "http://0.0.0.0:8000/api")
 QUEUE_CHANNEL_ID = int(get_secret("QUEUE_CHANNEL_ID", 1199195608091725954))
